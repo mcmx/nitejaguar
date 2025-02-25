3ea9e109a7382f4b42a7a703265acff92c7204dc
Sergio Guzman <sergio@sergioguzman.com>
2025-02-25 08:28:08 -0600

    rename again

--- internal/server/server.go
+++ internal/server/server.go
@@ -22,21 +22,21 @@ type Server struct {
 
 func NewServer(myDb database.Service, myTs actions.TriggerService) *http.Server {
 	port, _ := strconv.Atoi(os.Getenv("PORT"))
-	newServer := &Server{
+	intServer := &Server{
 		port: port,
 		db:   myDb,
 		ts:   myTs,
 	}
 	if port == 0 {
-		newServer.port = 8080
+		intServer.port = 8080
 		fmt.Println("No port specified. Using default port 8080")
 	} else {
-		fmt.Printf("Starting server on port %d\n", newServer.port)
+		fmt.Printf("Starting server on port %d\n", intServer.port)
 	}
 	// Declare Server config
 	server := &http.Server{
-		Addr:         fmt.Sprintf(":%d", newServer.port),
-		Handler:      newServer.RegisterRoutes(),
+		Addr:         fmt.Sprintf(":%d", intServer.port),
+		Handler:      intServer.RegisterRoutes(),
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
