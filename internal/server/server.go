53a3d8a47fa4b08c46ed92b0db20d74443febddd
Sergio Guzman <sergio@sergioguzman.com>
2025-02-25 08:15:29 -0600

    rename of internal variable

--- internal/server/server.go
+++ internal/server/server.go
@@ -22,21 +22,21 @@ type Server struct {
 
 func NewServer(myDb database.Service, myTs actions.TriggerService) *http.Server {
 	port, _ := strconv.Atoi(os.Getenv("PORT"))
-	NewServer := &Server{
+	newServer := &Server{
 		port: port,
 		db:   myDb,
 		ts:   myTs,
 	}
 	if port == 0 {
-		NewServer.port = 8080
+		newServer.port = 8080
 		fmt.Println("No port specified. Using default port 8080")
 	} else {
-		fmt.Printf("Starting server on port %d\n", NewServer.port)
+		fmt.Printf("Starting server on port %d\n", newServer.port)
 	}
 	// Declare Server config
 	server := &http.Server{
-		Addr:         fmt.Sprintf(":%d", NewServer.port),
-		Handler:      NewServer.RegisterRoutes(),
+		Addr:         fmt.Sprintf(":%d", newServer.port),
+		Handler:      newServer.RegisterRoutes(),
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
